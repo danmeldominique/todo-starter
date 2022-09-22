72795ffb5cb4f211460ffea76b8a5c0a4368f9a1
Daniel Dominique <danmeldominique@gmail.com>
2022-09-22 14:31:31 +1000

    add css

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react'
 import '../static/style.css'
 import TodoItem from './TodoItem'
+import { PlusIcon } from '@heroicons/react/24/outline'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [text, setText] = useState("")
   const [todoItems, setTodoItems] = useState([])
-  const [message, setMessage] = useState("")
+
+  const notify = () => {
+    if (text.length > 0) {
+      toast.success("Added to list", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    } else {
+      toast.error("There is no text in the box. Not adding to list...", {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
+    }
+  };
+
   const renderItems = () => {
     return todoItems.map(todo => {
       return (
@@ -15,13 +30,11 @@ const App = () => {
   }
 
   const addItem = (item) => {
-    if (text.length > 0 ) {
-      setTodoItems([...todoItems,item])
+    if (text.length > 0) {
       setText("")
-    }
-    else {
-      setMessage("There is no text in the box. Not adding to list...")
-    }
+      setTodoItems([...todoItems, item])  
+    }   
+    notify()
   }
 
   const removeItem = (id) => {
@@ -29,17 +42,34 @@ const App = () => {
   }
 
   return (
-    <div>
-      <div>
-        <input className='border-2 border-black' type="text" name="todo-input" id="todo-input" value={text} onChange={e => setText(e.target.value)} />
-        <button onClick={() => addItem({id: todoItems.length, text: text})}>Add</button>
-        <p className="message">{message}</p>
+<div className=' grid h-screen place-items-center'>
+<div className='container mx-auto w-1/4'>
+      <div className='text-center'>
+        <p className='tag font-bold text-2xl'>Jot It!</p>
+        <p className='sub-tag mb-5'>A basic todo app.</p>
+      </div>
+      <div className="text-center">
+        <div className='flex mx-auto drop-shadow-md'>
+          <input className='flex-auto' type="text" name="todo-input" id="todo-input" value={text} onChange={e => setText(e.target.value)} />
+          <button className='bg-green-500 flex-none p-2' onClick={() => addItem({ id: todoItems.length, text: text })}><PlusIcon className='h-8 w-8 text-white' /></button>
+        </div>
       </div>
 
-      <div className="todo-items">
+      <div className="todo-items pt-5">
         {renderItems()}
       </div>
+      <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover />
     </div>
+</div>
 
   )
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import { TrashIcon } from '@heroicons/react/24/outline'
 const TodoItem = ({id, text, removeItem}) => {
   return (
-    <div className='flex'>
-        <p className="counter">{id+1}</p>
-        <p className="todo-text">{text}</p>
-        <button className="delete" onClick={() => removeItem(id)}>Remove</button>
+    <div className='flex drop-shadow-lg my-2'>
+        <span className="counter flex-none px-5 my-auto">{id+1}</span>
+        <span className="todo-text grow hrink-0 my-auto"><p className="h-8 w-8">{text}</p></span>
+        <button className="delete flex-none bg-red-500 p-2" onClick={() => removeItem(id)}><TrashIcon className='h-8 w-8 text-white' /></button>
     </div>
   )
 }
